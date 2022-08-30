74a51318538984b315f310ab250ab6a01596ddeb
damianlips <damianlips87@gmail.com>
2022-08-30 18:49:10 -0300

    primer commit

---
+++ src/test/java/isi/dan/guias/pedidos/PedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package isi.dan.guias.pedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
